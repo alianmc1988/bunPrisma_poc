5c0c407822fb50e15ed0a66ceae67cd03d4d42bf
Alian Medina <alianmc1988@gmail.com>
2024-05-07 17:11:41 -0300

    initial commit

---
+++ src/external/config.ts
@@ -0,0 +1,5 @@
+import Bun from 'bun'
+
+export default {
+	port: Bun.env.PORT || 3000,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia'
+import Config from './external/config'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get('/', () => 'Hello Elysia').listen(Config.port)
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+)
